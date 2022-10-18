2ceabdea1bf7fddc4a708d965c1586339d8b1806
hello <1324977568@qq.com>
2022-10-18 22:30:04 +0800

    third commit

--- src/main/java/org/example/NewJava.java
+++ src/main/java/org/example/NewJava.java
@@ -4,5 +4,6 @@ public class NewJava {
     public static void main(String[] args) {
         System.out.println(1234556);
         System.out.println("改过了");
+        System.out.println("第二次修改了");
     }
 }
